6552642c0889d3f7aabb6660a4543abf6deffe6b
Kensei-Sama <52254166+Kensei-Sama@users.noreply.github.com>
2020-11-01 23:19:08 +0100

    update

---
+++ ult_bal_final/ult_bal_final/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ult_bal_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzők> versenylista = new List<Versenyzők>();
+            string[] adatok = File.ReadAllLines("ub2017egyeni.txt");
+            for (int i = 1; i < adatok.Length; i++)
+            {
+                versenylista.Add(new Versenyzők(adatok[i]));
+            }
+
+            //3. feladat
+            Console.WriteLine($"3. Feladat: Egyéni indulók: {versenylista.Count} fő");
+
+            //4. feladat
+            int nok = 0;
+            for (int i = 0; i < versenylista.Count; i++)
+            {
+                if (versenylista[i].Kategoria == "Női" && versenylista[i].Tavszazalek == 100)
+                {
+                    nok++;
+                }
+            }
+            Console.WriteLine($"4. Feladat: Célba érkező női sportolók: {nok} fő");
+
+            //5. feladat
+            string nev;
+            int seged = 0;
+            Console.WriteLine("Adja meg a sportoló nevét: ");
+            nev = Console.ReadLine();
+            while (seged < )
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ult_bal_final/ult_bal_final/Versenyzők.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ult_bal_final
+{
+    class Versenyzők
+    {
+        //Versenyzo;Rajtszam;Kategoria;Versenyido;TavSzazalek
+        string versenyzonev;
+        string rajtszam;
+        string kategoria;
+        int ora;
+        int perc;
+        int masodperc;
+        int tavszazalek;
+        string[] adat;
+        string[] seged; 
+
+        public Versenyzők(string y)
+        {
+            adat = y.Split(';');
+            Versenyzonev = adat[0];
+            Rajtszam = adat[1];
+            Kategoria = adat[2];
+            seged = adat[3].Split(':');
+            Ora = int.Parse(seged[0]);
+            Perc = int.Parse(seged[1]);
+            Masodperc = int.Parse(seged[2]);
+            Tavszazalek = Convert.ToInt32(adat[4]);
+        }
+
+        public string Versenyzonev { get => versenyzonev; set => versenyzonev = value; }
+        public string Rajtszam { get => rajtszam; set => rajtszam = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+        public int Ora { get => ora; set => ora = value; }
+        public int Perc { get => perc; set => perc = value; }
+        public int Masodperc { get => masodperc; set => masodperc = value; }
+        public int Tavszazalek { get => tavszazalek; set => tavszazalek = value; }
+    }
+}
